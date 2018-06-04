39e4fdd95d8b5e541ffed15ce98733e9af167c19
SashaRoyenko <36455593+SashaRoyenko@users.noreply.github.com>
2018-06-04 21:30:49 +0300

    partner section

--- bootstrap project/js/common.js
+++ bootstrap project/js/common.js
@@ -12,4 +12,40 @@ function stickyHeader(){
 
 $(window).on('scroll', function(){
     stickyHeader();
-})
+});
+
+$(document).ready(function(){
+    var owlMain = $('.main-slider'),
+        owlTwo = $('.second-slider'),
+        owlThree = $('.partners_s__slider');
+    owlTwo.owlCarousel({
+        items:3,
+        loop: true,
+        nav:false,
+        dots:false,
+        margin:30,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        autoplaySpeed: 2000,
+        dragEndSpeed: 2000
+    });
+    $('.second_slider_next').click(function(){
+        owlTwo.trigger("next.owl.carousel");
+    });
+    $('.second_slider_prev').click(function(){
+        owlTwo.trigger("prev.owl.carousel");
+    });
+    owlThree.owlCarousel({
+        items:4,
+        loop: true,
+        nav:false,
+        dots:false,
+        margin:90,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        autoplaySpeed: 2000,
+        dragEndSpeed: 2000
+    })
+});
